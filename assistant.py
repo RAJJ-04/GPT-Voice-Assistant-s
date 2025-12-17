31ee1f34663751204628b74c099a384d0288e10e
Raj Jarad <rajjarad04@gmail.com>
2025-12-17 19:01:32 +0530

    Update assistant.py

---
+++ assistant.py
@@ -0,0 +1,12 @@
+from os import system
+import speech_recognition as sr
+from gpt4all import GPT4All
+import sys
+import whisper
+import warnings
+import time
+import pyautogui
+import webbrowser
+import os
+
+model = GPT4All
