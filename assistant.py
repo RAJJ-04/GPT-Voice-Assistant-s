6ecaf09c985023de50d6299230d5542f529a28f9
Raj Jarad <rajjarad04@gmail.com>
2025-12-25 23:48:47 +0530

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -9,5 +9,28 @@ import pyautogui
 import webbrowser
 import os
 
-model = GPT4All
-print(model)  
+model = GPT4All("C:/Users/cptli/Documents/dev/Python", allow_download=False)
+assistant_name = "max"
+listening_for_trigger_word = True
+should_run = True
+source = sr.Microphone()
+recognizer = sr.Recognizer()
+base_model_path = os.path.expanduser('~/.cache/whisper/base.pt')
+base_model = whisper.load_model(base_model_path)
+
+if sys.platform != 'darwin':
+    import pyttsx3
+    engine = pyttsx3.init() 
+
+tasks = []
+listeningToTask = False
+askingAQuestion = False
+
+def respond(text):
+    if sys.platform == 'darwin':
+        ALLOWED_CHARS = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,?!-_$:+-/ ")
+        clean_text = ''.join(c for c in text if c in ALLOWED_CHARS)
+        system(f"say '{clean_text}'")
+    else:
+        engine.say(text)
+        engine.runAndWait()
